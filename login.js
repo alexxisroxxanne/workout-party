f3aea04d6e26bf47aeb9407140960797acc6c70b
Alexxis Johnson <alexxis.r.p.johnson@gmail.com>
2017-05-07 21:05:41 -0700

    Turn login into a module

--- login.js
+++ login.js
@@ -1,14 +1,10 @@
 const nightmare = require('nightmare')( {show: true} );
 
-const user = require('./user-info');
+let login = (user) => {
+  return nightmare.goto('https://classpass.com/login')
+    .type('#email_field', user.email)
+    .type('#password_field', user.password)
+    .click('button[type="submit"]')
+}
 
-nightmare.goto('https://classpass.com/login')
-         .type('#email_field', user.email)
-         .type('#password_field', user.password)
-         .click('button[type="submit"]')
-         .evaluate( () => document.querySelector('.hero__title') )
-         .end()
-         .then( (data) => {
-            console.log(data)
-         })
-         .catch( (err) => console.error(err) );
+module.exports = login;
