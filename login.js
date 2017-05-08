62dd42b5a620a27bd2f0a7de9a5040c358c88ead
Alexxis Johnson <alexxis.r.p.johnson@gmail.com>
2017-05-07 18:44:52 -0700

    Refactor login code

--- login.js
+++ login.js
@@ -1,18 +1,14 @@
-var Nightmare = require('nightmare'); 
-var user = require('./user-info'); 
-var nightmare = Nightmare({show: true}); 
+const nightmare = require('nightmare')( {show: true} );
+
+const user = require('./user-info');
 
 nightmare.goto('https://classpass.com/login')
          .type('#email_field', user.email)
          .type('#password_field', user.password)
          .click('button[type="submit"]')
-         .evaluate(function() {
-            return document.querySelector('.hero__title')
-         })
+         .evaluate( () => document.querySelector('.hero__title') )
          .end()
-         .then(function(data) {
+         .then( (data) => {
             console.log(data)
          })
-         .catch(function(err) {
-           return console.error(err)
-         })
+         .catch( (err) => console.error(err) );
